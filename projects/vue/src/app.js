6211e059be115fd702c98db8447fe2bdd3a8135f
Jesica Martin <jesmrtn11@gmail.com>
2018-07-20 00:28:35 +0200

    Created a vue app

--- projects/vue/src/app.js
+++ projects/vue/src/app.js
@@ -1,1 +1,14 @@
-alert("vue");
+import Vue from "vue";
+
+new Vue({
+	el: "#app",
+	data: {
+		title: "Vue title"
+	},
+	methods: {
+
+	}, 
+	template: `
+		<div class="vue-title"><h1>{{ title }}</h1></div>
+	`
+});
